092ffec74d2d4b5056beb81a4d63d7892e2771ed
Drago Boss <dragoboss@live.nl>
2017-04-05 05:50:07 -0700

    no message

---
+++ src/main/java/me/vik1395/ProtectionStones/ObjectOrientated/PSRegionSettings.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.vik1395.ProtectionStones.ObjectOrientated;
+
+import java.util.List;
+import org.bukkit.Material;
+
+/**
+ *
+ * @author Mirco Swaders
+ */
+public class PSRegionSettings {
+ 
+    List<?> getTypes(Material m) {
+        List<?> types = null;
+        return types;
+    }
+    
+}
